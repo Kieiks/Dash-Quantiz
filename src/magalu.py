9382515404ec71039631a399695a61d7259b51b2
Kieiks <kieiks@gmail.com>
2024-08-05 14:28:25 -0300

    ajuste magalu 2

--- src/magalu.py
+++ src/magalu.py
@@ -31,32 +31,32 @@ def get_price(price_raw):
     return price_object.amount_float
 
 
-# def request_url(q,pagina):
-#     targetUrl = urllib.parse.quote(f"https://www.amazon.com.br/s?k={generate_url(q)}&page={pagina}")
-#     url = "http://api.scrape.do?token={}&url={}&geoCode=br".format(token, targetUrl)
-#     r = requests.request("GET", url)
+def request_url(q,pagina):
+    targetUrl = urllib.parse.quote(f"https://www.magazineluiza.com.br/busca/{generate_url(q)}/?from=submit&page={pagina}")
+    url = "http://api.scrape.do?token={}&url={}&geoCode=br".format(token, targetUrl)
+    r = requests.request("GET", url)
 
-#     return r.text
-
-def make_request(q,pagina):
-
-    headers = {
-            "accept": "*/*",
-            "accept-language": "pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,es-CL;q=0.5,es;q=0.4",
-            "priority": "u=1, i",
-            "referer": "https://www.magazineluiza.com.br/",
-            "sec-ch-ua-mobile": "?0",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
-            "x-nextjs-data": "1"
-        }
-
-    url = f'https://www.magazineluiza.com.br/busca/{generate_url(q)}/?from=submit&page={pagina}'
-
-    r = requests.get(url,headers=headers)
     return r.text
+
+# def make_request(q,pagina):
+
+#     headers = {
+#             "accept": "*/*",
+#             "accept-language": "pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,es-CL;q=0.5,es;q=0.4",
+#             "priority": "u=1, i",
+#             "referer": "https://www.magazineluiza.com.br/",
+#             "sec-ch-ua-mobile": "?0",
+#             "sec-fetch-dest": "empty",
+#             "sec-fetch-mode": "cors",
+#             "sec-fetch-site": "same-origin",
+#             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
+#             "x-nextjs-data": "1"
+#         }
+
+#     url = f'https://www.magazineluiza.com.br/busca/{generate_url(q)}/?from=submit&page={pagina}'
+
+#     r = requests.get(url,headers=headers)
+#     return r.text
 
 def extract_data(data):
     selector = Selector(data)
@@ -83,8 +83,8 @@ def magalu(q,paginas):
     if remaining_request():
         results = []
         for pagina in range(1,paginas+1):
-            # data = request_url(q,pagina)
-            data = make_request(q,pagina)
+            data = request_url(q,pagina)
+            # data = make_request(q,pagina)
             l = extract_data(data)
             results = list(itertools.chain(results, l))
 
